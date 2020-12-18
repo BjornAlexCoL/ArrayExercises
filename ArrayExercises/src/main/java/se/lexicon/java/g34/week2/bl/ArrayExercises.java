fe28e4fb8482ab9309dbf6b3e3ee306bff565a72
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2020-12-18 11:55:33 +0100

    Add files via upload

--- ArrayExercises/src/main/java/se/lexicon/java/g34/week2/bl/ArrayExercises.java
+++ ArrayExercises/src/main/java/se/lexicon/java/g34/week2/bl/ArrayExercises.java
@@ -1,21 +1,20 @@
 package se.lexicon.java.g34.week2.bl;
 
+
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayExercises {
     public static void main(String[] args) {
-        String[] menu = {"1. Exercise 1", "2. Exercise 2", "3. Exercise 3", "4. Exercise 4",
-                "5. Exercise 5", "0. Quit"};
-        Scanner inp = new Scanner(System.in);
+        String[] menu = {"\nExercise Menu","1.\tArray Exercise 1", "2.\tArray Exercise 2", "3.\tArray Exercise 3", "4.\tArray Exercise 4",
+                "5.\tArray Exercise 5", "6.\tArray Exercise 6", "7.\tArray Exercise 7", "8.\tArray Exercise 8","9.\tArray Exercise 9",
+                "10.\tArray Exercise 10","11.\tArray Exercise 11", "12.\tArray Exercise 12", "13.\tArray Exercise 13", "0.\tQuit"};
         int menuInput;
+        printMenu(menu);
         do {
-            System.out.println("\nExercise Menu");
-            for (String menuIndex : menu) {
-                System.out.println(menuIndex);
-            }
-            System.out.print("What Exercise do you want to run?:");
-            menuInput = inp.nextInt();
+            System.out.print("\nWhat Exercise do you want to run?:");
+            menuInput = getValidInt();//Reused from Exercise 9
             switch (menuInput) {
                 case 0: {
                     System.out.println("Thx for this time. Catch you later");
@@ -45,15 +44,56 @@ public class ArrayExercises {
                     exercise5(locations);
                     break;
                 }
+                case 6: {
+                    int[] valueArray = {43, 5, 23, 17, 2, 14};
+                    exercise6(valueArray);
+                    break;
+                }
+                case 7: {//Excercise 7
+                    int[] intArray = {1, 2, 4, 7, 9, 12};
+                    exercise7(intArray);
+                    break;
+                }
+                case 8: {//Exercise 8
+                    exercise8();
+                    break;
+                }
+                case 9: {
+                    exercise9();
+                    break;
+                }
+                case 10: { //Exercise 10
+                    exercise10();
+                    break;
+                }
+                case 11: { //Exercise 11
+                    exercise11();
+                    break;
+                }
+                case 12: {
+                    exercise12();
+                    break;
+                }
+                case 13: {
+                    exercise13();
+                    break;
+                }
                 default: {
+                    printMenu(menu);
                     break;
                 }
             }
+
         }
         while (menuInput != 0);
     }
 
+    public static void printMenu(String[] menu){
+        for (String menuIndex : menu) {
+            System.out.println(menuIndex);
+        }
 
+    }
     /**
      * Write a program which will store elements in an array of type ‘int’ and print it out.
      * Expected output: 11 23 39 etc.
@@ -73,17 +113,17 @@ public class ArrayExercises {
         Expected output: Index position of number 5 is: 2.
     */
     public static void exercise2(int[] list) {
-        Scanner inp=new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         System.out.print("Input number to search for:");
         int iSearch = inp.nextInt();
         int index = indexOf(list, iSearch);
         if (index == -1) {
             System.out.println(iSearch + " does not exist in the list");
-            }
-            else {
-                System.out.println("Index position of number " + iSearch + " is: " + index);
-                }
+        } else {
+            System.out.println("Index position of number " + iSearch + " is: " + index);
         }
+    }
+
     public static int indexOf(int[] list, int find) {
         for (int index = 0; index < list.length; index++) {
             if (list[index] == find) {
@@ -116,6 +156,7 @@ public class ArrayExercises {
             }
         }
     }
+
     /* 4. Write a program which will copy the elements of one array into another array.
     Expected output: Elements from first array: 1 15 20
     Elements from second array: 1 15 20 */
@@ -123,7 +164,7 @@ public class ArrayExercises {
         int[] copiedNumbers = Arrays.copyOf(numbers, numbers.length);
         printInt(numbers, "Elements from first array:", " ", " ", "");
         printInt(copiedNumbers, "Elements from second array:", " ", " ", "");
-        }
+    }
 
     public static void printInt(int[] printArray, String label, String prefix, String seperator, String epifix) {
         System.out.print("\n" + label + prefix);
@@ -148,11 +189,12 @@ public class ArrayExercises {
         String[][] locations = new String[list.length / 2][2];
         int listIndex = 0;
         for (String postValue : list) {
-            locations[listIndex/2][listIndex % 2] = postValue;
+            locations[listIndex / 2][listIndex % 2] = postValue;
             listIndex++;
         }
         printStringArray(locations);
     }
+
     //print exercise5
     public static void printStringArray(String[][] printArray) {
         System.out.print("\n");
@@ -168,9 +210,272 @@ public class ArrayExercises {
         }
     }
 
+    /*6. Write a program which will set up an array to hold the next values in this
+    order: 43, 5, 23, 17, 2, 14 and print the average of these 6 numbers.
+    Expected output:
+    Average is: 17.3*/
+    public static void exercise6(int[] numbers) {
+        int sum = totSum(numbers);//The sum of the numbers
+        int average = sum / numbers.length;//The integerpart of the average
+        int mod = sum * 10 / numbers.length % 10;//The decimal part we increase the sum with 10 for 1 decimal as we don't want to truncate it before getting the mod from it
+        System.out.print("Average is: " + average + "." + mod);
+    }
+
+    public static int totSum(int[] numbers) {
+        int sum = 0;
+        for (int index : numbers) {
+            sum += index;
+        }
+        return sum;
+    }
+
+    /*7. Write a program which will set up an array to hold 10 numbers and print
+     out only the uneven numbers.
+     Expected output:
+     Array: 1 2 4 7 9 12
+     Odd Array: 1 7 9*/
+    public static void exercise7(int[] originalArray) {
+        System.out.print("Array: ");
+        printIntegers(originalArray);
+        printOddIntegers(originalArray);
+    }
+
+    public static void printIntegers(int[] elementArray) {
+        for (int element : elementArray) {
+            System.out.print(element + " ");
+        }
+    }
+
+    public static void printOddIntegers(int[] checkArray) {
+        System.out.print("\nOdd Array: ");
+        for (int index : checkArray) {
+            if (index % 2 == 1) {
+                System.out.print(index + " ");
+            }
+        }
+    }
+    /*8. Write a program which will remove the duplicate elements of a given
+    array [20, 20, 40, 20, 30, 40, 50, 60, 50]. Expected output: Array: 20 20 40 20 30 40 50 60 50
+    Array without duplicate values: 20 40 30 50 60*/
+    public static void exercise8(){
+        int[] startArray={20,20,40,20,30,40,50,60,50};
+        int[] resultArray=new int[0];
+        for(int index:startArray) {
+            if (!doesExist(resultArray, index)){
+                resultArray=addElement(resultArray,index);//Reused from exercise 9
+            }
+        }
+        System.out.print("Array: ");
+        printIntegers(startArray);//reused from exercise 7
+        System.out.print("\nArray without duplicate values: ");
+        printIntegers(resultArray);//reused from exercise 7
+    }
+
+    public static boolean doesExist(int[] valueList, int seekValue) {
+        Arrays.sort(valueList);
+        if(Arrays.binarySearch(valueList, seekValue)<0) {
+            return false;
+        }
+         else {
+            return true;
+        }
+    }
+
+    /*9. Write a method which will add elements in an array.
+    Remember that arrays are fixed in size so you need to come up with a
+    solution to “expand” the array.*/
+    public static void exercise9() {
+        Scanner inp = new Scanner(System.in);
+        String quitCheck;
+        int[] elementArray = new int[0];
+        int newElement;
+        do {
+            System.out.print("Elements in this Excercise are supposesed to be an integer.\nPlease input element: ");
+            newElement = getValidInt();//input a valid integer
+            elementArray = addElement(elementArray, newElement);//Add the new element
+            printElementArray(elementArray); //Show us what we got
+            System.out.print("\nDo you want to stop entering elements?");
+            quitCheck = inp.nextLine();
+            if (quitCheck.isEmpty()) {
+                quitCheck = "N";
+                continue;
+            }
+            quitCheck = quitCheck.toLowerCase();
+        }
+        while (quitCheck.charAt(0) != 'y');
+    }
+
+    public static int getValidInt() {
+        Scanner inp = new Scanner(System.in);
+        String testValid;
+        boolean isValid;
+        do {
+            isValid = true;
+            testValid = inp.nextLine();
+            if (testValid.isEmpty()) { /* if testValid is empty */
+                System.out.println("How do you suppose I should work when not entering an integer? ");
+                System.out.print("Try to input an Integer again, please:");
+                isValid = false;
+                continue;
+            }
+            try {//Check if it's and valid integer
+                Integer.parseInt(testValid);
+            } catch (NumberFormatException e) {
+                System.out.println("Since when did " + testValid + " become an integer.");
+                System.out.print("Try to input an Integer again, please:");
+                isValid = false;
+            }
+        }
+        while (!isValid);//loop until we get a valid integer.
+        return Integer.parseInt(testValid);
+    }
+
+    public static int[] addElement(int[] oldArray, int element) {
+        int[] newArray;
+        newArray = Arrays.copyOf(oldArray, oldArray.length + 1);
+        newArray[newArray.length - 1] = element;
+        return newArray;
+    }
+
+    public static void printElementArray(int[] elementArray) {
+        for (int element : elementArray) {
+            System.out.print(element + "\t");
+        }
+    }
+
+    /*10. Write a program which will represent multiplication table stored in
+    multidimensional array.
+    Hint: You have two-dimensional array with values
+    [[1,2,3,4,5,6,7,8,9,10], [1,2,3,4,5,6,7,8,9,10]]*/
+    public static void exercise10() {
+        printEasyTable(16); //if we just want to have a table
+        int[][] productMatris = assignProductMatris(10);//assign the two dimentional array
+        printArrayTable(productMatris); //print it.
+    }
+
+    public static int[][] assignProductMatris(int size) {
+        int[][] productMatris = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                productMatris[row][col] = (row + 1) * (col + 1);
+            }
+        }
+
+        return productMatris;
+    }
+
+    public static void printArrayTable(int[][] productArray) {
+        System.out.println();
+        for (int[] row : productArray) {
+            for (int col : row) {
+                System.out.print(col);
+                System.out.print("\t\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printEasyTable(int size) {
+        for (int row = 1; row <= size; row++) {
+            for (int col = 1; col <= size; col++) {
+                System.out.print(row * col);
+                System.out.print("\t\t");
+            }
+            System.out.println();
+        }
+    }
+    /*11. Write a program that ask the user for an integer and repeat that
+     question until user give you a specific value that user already has been
+     told about as a message in your program. Store these values in an array
+     and print that array. After that reverse the array elements so that the
+     first element becomes the last element, the second element becomes the second to last
+     element, etc. Do not just reverse the order in which
+     hey are printed. You need to change the way they are stored in the
+      array.
+     */
+    public static void exercise11()
+    {
+        Random rnd = new Random();
+        int secretNumber=rnd.nextInt(10);//Random number between 0-9
+        int[] guesses=new int[0];
+        int currentGuess;
+        System.out.println("The secret number is "+secretNumber);
+        do {
+            System.out.print("Please enter an Integer: ");
+            currentGuess=getValidInt(); //Reused from Exercise 9
+            guesses = addElement(guesses, currentGuess);//reused from Exercise 9
+        }
+        while (currentGuess!=secretNumber);
+        System.out.println("\nguesslist");
+        printIntegers(guesses); //reused from Exercise 7
+        guesses=reverseArray(guesses);
+        System.out.println("\ninverted guesslist");
+        printIntegers(guesses); //reused from Exercise 7
+        }
+
+    public static int[] reverseArray(int[] orgArray){
+        int[] revArray=new int[orgArray.length];
+        for(int i=0;i<orgArray.length;i++){
+            revArray[revArray.length-i-1]=orgArray[i];
+            }
+        return revArray;
+
+    }
+    /*12. Write a program which will print the diagonal elements of
+    two-dimensional array.
+    Expected output: 1 4 9
+     */
+    public static void exercise12() {
+        int[][] sumMatris = assignProductMatris(3);//Reuse from exersize 10
+        printArrayDiagonal(sumMatris); //print it.
+
+    }
+
+    public static void printArrayDiagonal(int[][] diagonalArray) {
+        System.out.println("\n");
+        for (int diagonal = 0; diagonal < diagonalArray.length; diagonal++) {
+            System.out.print(diagonalArray[diagonal][diagonal] + "\t");
+        }
+    }
+
+    /*13. Create two arrays with arbitrary size and fill one with random numbers.
+    Then copy over the numbers from the array with random numbers so
+    that the even numbers are located in the rear (the right side) part of the
+    array and the odd numbers are located in the front part (the left side).*/
+    public static void exercise13() {
+        System.out.print("\nHow many random numbers do you want to work with?: ");
+        int amount = getValidInt();
+        System.out.print("\nWhat will be the highest value int the array?");
+        int highValue = getValidInt();
+        int[] randomArray = assignArrayWithRandoms(amount, highValue);
+        int[][] sortedArray = oddEvenSortArray(randomArray);
+        printArrayTable(sortedArray);
+    }
+
+    public static int[] assignArrayWithRandoms(int amount, int highValue) {
+        int[] rndArray = new int[0];
+        Random rnd = new Random();
+        for (int index = 0; index < amount; index++) {
+            rndArray = addElement(rndArray, rnd.nextInt(highValue) + 1);
+            System.out.print(rndArray[index] + " ");
+        }
+        return rndArray;
+    }
+
+    public static int[][] oddEvenSortArray(int[] numList) {
+        int[][] sortMatris = new int[2][];
+        int[] evenArray=new int[0];
+        int[] oddArray=new int[0];
+        for (int number:numList) {
+                if (number % 2 == 1) {
+                    oddArray=addElement(oddArray, number);
+                }
+                else {
+                    evenArray=addElement(evenArray, number);
+                }
+            }
+        sortMatris[0]=Arrays.copyOf(oddArray,oddArray.length);
+        sortMatris[1]=Arrays.copyOf(evenArray,evenArray.length);
+        return sortMatris;
+    }
 }
-
-
-
-
-
